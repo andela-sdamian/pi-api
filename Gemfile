dc4d780f72c097ae58a92e924db0f9759f85053b
andela-sdamian <simon.peter@andela.com>
2016-08-11 21:02:14 +0100

    initial commit

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source 'https://rubygems.org'
+
+gem 'pg'
+gem 'rails', '~> 5.0.0'
+gem 'puma', '~> 3.0'
+
+
+
+group :development, :test do
+  gem 'byebug', platform: :mri
+  gem 'pry-rails'
+  gem 'sqlite3'
+end
+
+group :development do
+  gem 'listen', '~> 3.0.5'
+  gem 'spring'
+  gem 'spring-watcher-listen', '~> 2.0.0'
+end
+
+gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
---
+++ app/controllers/lights_controller.rb
@@ -0,0 +1,27 @@
+class LightsController < ApplicationController
+  before_action :set_light, only: [:show, :update]
+
+
+  def show
+    render json: @light
+  end
+
+
+  def update
+    if @light.update(light_params)
+      render json: @light
+    else
+      render json: @light.errors, status: :unprocessable_entity
+    end
+  end
+
+
+  private
+    def set_light
+      @light = Light.first
+    end
+
+    def light_params
+      params.permit(:on)
+    end
+end
