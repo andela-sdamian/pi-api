0e1598608df0d361489310397f99fa44c23c0f71
Femi Senjobi <femi.senjobi@andela.com>
2016-08-13 16:02:11 +0100

    turn heater on if temp reading goes below minimum

--- app/models/temperature.rb
+++ app/models/temperature.rb
@@ -10,8 +10,18 @@ class Temperature < ApplicationRecord
     Fan.first.update_attributes(on: false)
   end
 
+  def turn_heater_on
+    Heat.first.update_attributes(on: true)
+  end
+
+  def turn_heater_off
+    Heat.first.update_attributes(on: false)
+  end
+
   def check_setting
     turn_fan_on if reading > Setting.last.temp[:max]
     turn_fan_off if reading < Setting.last.temp[:min]
+    turn_heater_on if reading < Setting.last.temp[:min]
+    turn_heater_off if reading > Setting.last.temp[:max]
   end
 end
